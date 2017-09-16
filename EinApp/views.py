c7323b69fd5ef2a1547b21020e8654143d9817d0
Tor Stefan <torstefan@gmail.com>
2017-09-17 01:20:56 +0200

    Innlogging

--- EinApp/views.py
+++ EinApp/views.py
@@ -1,7 +1,12 @@
-from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import View
+
 from .models import Album
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'EinApp/index.html'
@@ -25,3 +30,37 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('EinApp:index')
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'EinApp/registration_form.html'
+
+    # display a blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            # cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns user objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('EinApp:index')
+
+        return render(request, self.template_name, {'form': form})
